70cc9ef28f0ff3e636dc3dc598960ca9e6ba64b4
Pablo <Pablo@192.168.1.38>
2018-10-15 21:41:45 +0100

    SEGUNDA VERSION

--- src/kata2_histograma/Kata2_Histograma.java
+++ src/kata2_histograma/Kata2_Histograma.java
@@ -7,8 +7,6 @@ package kata2_histograma;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
-
 
 /**
  *
@@ -24,29 +22,25 @@ public class Kata2_Histograma {
         int[] data;
         data = new int[30];
         
-        int LimMax = 9;
+        int LimMax = 15;
         int LimInf = 0;
         
         // Se inicializa el array de enteros con números enteros generados de manera aleatoria entre [LimInf,LimMax]: 
         for(int i = 0;i < data.length;i++){
             data[i] = (int) Math.floor(Math.random()*(LimInf-LimMax+1)+LimMax); 
         }
-        //prueba
-        //Se recorre el array data:
-        for(int i = 0;i < data.length;i++){
-            if(histogram.containsKey(data[i])) {
-                histogram.put(data[i], histogram.get(data[i])+1);
-            }else{
-                histogram.put(data[i], 1);
-            }
-        }
        
+        
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
+        }
         // Recorremos todos los elementos del mapa imprimiendo por consola cada clave 
         // con su valor correspondiente en el histograma
         histogram.keySet().stream().forEach((Integer key) -> {
             System.out.println(key+ "==>" + histogram.get(key));
         });
     }
+    
     
 }
 
